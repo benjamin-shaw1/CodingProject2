3ca63e4b459c3e7c9294b3cd23855a1f4eb8e166
Benjamin <benshaw77@hotmail.com>
2025-02-26 16:33:47 -0500

    uploaded base code,pdf, and utils file

---
+++ dataprocessing/utils.py
@@ -0,0 +1,28 @@
+import os 
+import sys
+from dotenv import load_dotenv, find_dotenv
+import panel as pn
+
+class utils:
+    def __init__(self):
+        pass
+    def get_api_key(self):
+        _ = load_dotenv(find_dotenv())
+        return os.getenv("API_Key")
+    
+    def get_api_url(self):
+        _ = load_dotenv(find_dotenv())
+        return os.getev("API_Url")
+class upld_file():
+    def __init__(self):
+        self.widget_file_upload = pn.widgets.FileInput(accept = '.pdf,.ppt.png.html.pptx', multiple = False)
+        self.widget_file_upload.param.watch(self.save_filename,"filename")
+        
+    def save_filename(self,_):
+        if len(self.widget_file_upload.value) >2e6:
+            print("file too large. 2 M limit")
+        else:
+            self.widget_file_upload.save('./example_files/'+self.widget_file_upload.filename)
+            
+        
+        
